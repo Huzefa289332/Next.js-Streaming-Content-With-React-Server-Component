f4d713c9b6439c57780be6585aefdcc7c6c05da2
Huzefa Battiwala <huzaifa.hb.123@gmail.com>
2023-11-21 20:17:29 +0500

    init

---
+++ src/app/snippets/[id]/edit/page.tsx
@@ -0,0 +1,26 @@
+import SnippetEditForm from '@/components/snippet-edit-form';
+import { db } from '@/db';
+import { notFound } from 'next/navigation';
+
+interface SnippetEditPageProp {
+  params: {
+    id: string;
+  };
+}
+
+export default async function SnippetEditPage(props: SnippetEditPageProp) {
+  const id = parseInt(props.params.id);
+  const snippet = await db.snippet.findFirst({
+    where: { id },
+  });
+
+  if (!snippet) {
+    notFound();
+  }
+
+  return (
+    <div>
+      <SnippetEditForm snippet={snippet} />
+    </div>
+  );
+}
