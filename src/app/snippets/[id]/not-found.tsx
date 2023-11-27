d8af3f7ef71374180a03dc1653a17a41dc73d180
Huzefa Battiwala <huzaifa.hb.123@gmail.com>
2023-11-27 09:42:04 +0500

    binding server actions with client component

--- src/app/snippets/[id]/not-found.tsx
+++ src/app/snippets/[id]/not-found.tsx
@@ -1,8 +1,8 @@
 export default function SnippetShowNotFound() {
   return (
     <div>
-      <h1 className="text-xl bold">
-        Sorry, but we could'nt find that particular snippet
+      <h1 className='text-xl bold'>
+        Sorry, but we could&apos;nt find that particular snippet
       </h1>
     </div>
   );
--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { db } from '@/db';
 import Link from 'next/link';
 import { notFound } from 'next/navigation';
+import * as actions from '@/actions';
 
 interface SnippetShowPageProps {
   params: {
@@ -17,23 +18,29 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
     return notFound();
   }
 
+  const deleteSnippetAction = actions.deleteSnippet.bind(null, snippet.id);
+
   return (
     <div>
-      <div className="flex m-4 justify-between items-center">
-        <h1 className="text-xl font-bold">{snippet.title}</h1>
+      <div className='flex m-4 justify-between items-center'>
+        <h1 className='text-xl font-bold'>{snippet.title}</h1>
 
-        <div className="flex gap-4">
+        <div className='flex gap-4'>
           <Link
             href={`/snippets/${snippet.id}/edit`}
-            className="p-2 border rounded"
+            className='p-2 border rounded'
           >
             Edit
           </Link>
-          <button className="p-2 border rounded">Delete</button>
+          <form action={deleteSnippetAction}>
+            <button type='submit' className='p-2 border rounded'>
+              Delete
+            </button>
+          </form>
         </div>
       </div>
 
-      <pre className="p-3 border rounded bg-gray-200 border-gray-200">
+      <pre className='p-3 border rounded bg-gray-200 border-gray-200'>
         <code>{snippet.code}</code>
       </pre>
     </div>
